908fda417d0929735c5c47f617d637302838478e
Jairo Calcina <j.calcina.v@gmail.com>
2025-07-04 23:12:00 -0400

    feat: modify server initialization

--- server.js
+++ server.js
@@ -4,23 +4,6 @@ const { port, serverUrl } = require("./src/config/env");
 const { ServerCredentials } = require("@grpc/grpc-js");
 const Email = require("./src/services/emailService");
 
-const userEmail = 'test@test.com';
-const bill = {
-  id: '12345',
-  amount: 100,
-  dueDate: '2023-12-31'
-};
-// Send a test email
-(async () => {
-  try {
-    await new Email({ email: userEmail })
-      .sendUpdateBillEmail(bill);
-    console.log(`Test email sent to ${userEmail} for bill update: ${bill.id}`);
-  } catch (err) {
-    console.error("Error sending test email:", err);
-  }
-})();
-
 server.bindAsync(
   `${serverUrl}:${port}`,
   ServerCredentials.createInsecure(),
--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const { Server } = require("@grpc/grpc-js");
-const { grpcErrorHandler } = require("./middlewares/grpcErrorHandlerMiddleware");
-// const initializeConsumers = require("./rabbitmq/initialize");
+const {
+  grpcErrorHandler,
+} = require("./middlewares/grpcErrorHandlerMiddleware");
+const initializeConsumers = require("./rabbitmq/initialize");
 
 const server = new Server();
 
 grpcErrorHandler(server);
-// initializeConsumers().then(() => console.log("🐇 Consumers ready"));
+initializeConsumers().then(() => console.log("🐇 Consumers ready"));
 
 module.exports = server;
